55d70eff4bec22eedbaf3eada472a38a2127e82e
TheOverclockedLad <42649194+TheOverclockedLad@users.noreply.github.com>
2019-01-12 11:53:22 +0530

    Add project files.

---
+++ MvcOnlineTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcOnlineTest.Startup))]
+namespace MvcOnlineTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
